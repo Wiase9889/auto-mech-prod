7a89ec78a9eeda79fafe7dcdb6cdf104cd7ad30b
wiase <kelvinakwettey@gmail.com>
2022-10-04 04:23:01 +0000

    all pages

--- src/Pages/Customers/Customers.jsx
+++ src/Pages/Customers/Customers.jsx
@@ -1,73 +1,59 @@
-/* eslint-disable no-restricted-globals */
-/* eslint-disable no-unused-vars */
-import React, { useLayoutEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import classes from './Customers.module.css'
 import Sidebar from '../../Components/Sidebar/Sidebar'
-import { Link } from 'react-router-dom'
 import db from '../../DB/db'
+import { Link } from 'react-router-dom'
+import TableBody from '../../Components/Table/TableBody'
 
 const Customers = () => {
-  const [customers, setCustomers] = useState([])
+  const [customers, setcustomers] = useState([])
 
-  useLayoutEffect(() => {
-    // Function to add the user inputed values to the Database...
-    let unsubscribe = () => {
-      db.collection('customers').get().then(customers => {
-        setCustomers(customers)
+  useEffect(() => {
+    let unsub = () => {
+      db.collection('customers').get().then(allcustomers => {
+        setcustomers(allcustomers)
       })
     }
 
     return () => {
-      unsubscribe()
-    }
-  }, [])
+      unsub()
+    };
+  }, [customers])
 
-  const handleDelete = (id) => {
-    const confirmDelete = confirm('Are you sure?');
-
-    if (confirmDelete) {
-      db.collection('customers').doc({ id }).delete();
-      alert(`Successfully removed ${id}`);
-
-      // Refetch the customers data
-      db.collection('customers').get();
-    } else {
-      return
-    }
-  }
   return (
     <>
       <Sidebar />
-      <div className={classes.customers}>
-        <div className={classes.customer_hd}>
+      <div className={classes.customers__page}>
+        <div className={classes.customers__hd}>
           <h2>Customers</h2>
         </div>
 
-        <div className={classes.addCustomerBtn}>
-          <Link to='/new-customer'>Add Customer</Link>
+        <div className={classes.addcustomersBtn}>
+          <Link to='/new-customer'>Add customer</Link>
         </div>
 
-        {/* Display all Customers */}
-        <div className={classes.contact_display}>
-          {
-            customers?.length === 0 ? (
-              <div className={classes.empty}>
-                <h2>Sorry, no customer available</h2>
-              </div>
-            ) :
-              customers?.map(({ name, id, contact }) => (
-                <div key={id} className={classes.contact}>
-                  <p>{id}</p>
-                  <h2>{name}</h2>
-                  <h2>{contact}</h2>
-                  <br />
-                  <div className={classes.controls}>
-                    <Link to={`/edit-customer/${id}`}>Edit</Link>
-                    <button type='button' onClick={() => handleDelete(id)}>Delete</button>
-                  </div>
+        {/* Display all products */}
+        <div className={classes.customers_display}>
+          <div className={classes.table}>
+            <th>
+              <tr>Id</tr>
+              <tr>Customer's Name</tr>
+              <tr>Contact</tr>
+              <tr>Controls</tr>
+            </th>
+          </div>
+          <div className={classes.customer__lists}>
+            {
+              customers?.length === 0 ? (
+                <div className={classes.empty}>
+                  <h2>Sorry, no customer available</h2>
                 </div>
-              ))
-          }
+              ) :
+                customers?.map((data) => (
+                  <TableBody data={data} />
+                ))
+            }
+          </div>
         </div>
       </div>
     </>
